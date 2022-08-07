7ce6fedd4329250871004ee0eac6fb478e0deb79
Junior <eecfredes@gmail.com>
2022-08-06 21:47:23 -0300

    feat: sql connection - test model fail, need to looking for it

--- Backend/src/_tests/model_db.rs
+++ Backend/src/_tests/model_db.rs
@@ -4,5 +4,9 @@ use super::init_db;
 async fn model_db_init_db() -> Result<(), Box<dyn std::error::Error>> {
     // ACTION
     let db = init_db().await?;
+
+    // CHECK
+    let result = sqlx::query("SELECT * from todo").fetch_all(&db).await?;
+    assert_eq!(2, result.len(), "number of seed todos");
     Ok(())
 }
--- Backend/src/model/db.rs
+++ Backend/src/model/db.rs
@@ -1,6 +1,7 @@
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
 use std::fs;
+use std::path::PathBuf;
 use std::time::Duration;
 
 const PG_HOST: &str = "localhost";
@@ -19,11 +20,34 @@ const SQL_RECREATE: &str = "sql/00-recreate-db.sql";
 pub type Db = Pool<Postgres>;
 
 pub async fn init_db() -> Result<Db, sqlx::Error> {
-    new_db_pool(PG_HOST, PG_ROOT_DB, PG_ROOT_USER, PG_ROOT_PWD, 1).await
+    // -- Create the db with PG_ROOT (dev only)
+    {
+        let root_db = new_db_pool(PG_HOST, PG_ROOT_DB, PG_ROOT_USER, PG_ROOT_PWD, 1).await?;
+        pexec(&root_db, SQL_RECREATE).await?;
+    }
+
+    // -- Run the app sql files
+    let app_db = new_db_pool(PG_HOST, PG_APP_DB, PG_APP_USER, PG_APP_PWD, 1).await?;
+    let mut paths: Vec<PathBuf> = fs::read_dir(SQL_DIR)?
+        .into_iter()
+        .filter_map(|e| e.ok().map(|e| e.path()))
+        .collect();
+    paths.sort();
+    // execute each file
+    for path in paths {
+        if let Some(path) = path.to_str() {
+            // only .sql and not the recreate
+            if path.ends_with(".sql") && path != SQL_RECREATE {
+                pexec(&app_db, &path).await?;
+            }
+        }
+    }
+
+    // returning the app db
+    new_db_pool(PG_HOST, PG_APP_DB, PG_APP_USER, PG_APP_PWD, PG_APP_MAX_CON).await
 }
 
 async fn pexec(db: &Db, file: &str) -> Result<(), sqlx::Error> {
-
     // Read the file
     let content = fs::read_to_string(file).map_err(|ex| {
         println!("ERROR reading {} (cause: {:?})", file, ex);
@@ -36,7 +60,7 @@ async fn pexec(db: &Db, file: &str) -> Result<(), sqlx::Error> {
     for sql in sqls {
         match sqlx::query(&sql).execute(db).await {
             Ok(_) => (),
-            Err(ex) => println!("WARNIG - pexec - Sql '{}' FAILED cause: {}", file, ex),
+            Err(ex) => println!("WARNING - pexec - Sql file '{}' FAILED cause: {}", file, ex),
         }
     }
 
@@ -53,11 +77,13 @@ async fn new_db_pool(
     let con_string = format!("postgres://{}:{}@{}/{}", user, pwd, host, db);
     PgPoolOptions::new()
         .max_connections(max_con)
-        .connect_timeout(Duration::from_millis(500))
+        .acquire_timeout(Duration::from_millis(500)) // Needs to find replacement
         .connect(&con_string)
         .await
 }
 
+// region:    Test
 #[cfg(test)]
 #[path = "../_tests/model_db.rs"]
 mod tests;
+// endregion: Test
